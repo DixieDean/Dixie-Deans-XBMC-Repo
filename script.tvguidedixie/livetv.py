6d6fd42f2cbeaef9eaee4155d4600754ceddb25b
DixieDean <write2dixie@gmail.com>
2016-03-16 03:29:05 +0000

    OTEPG 3.3.8

--- script.tvguidedixie/livetv.py
+++ script.tvguidedixie/livetv.py
@@ -9,16 +9,16 @@ l1ll1 = 7
 def l1ll1l (ll):
 	global l1l1ll
 	
-	l11111 = ord (ll [-1])
-	l1111 = ll [:-1]
+	l111l1 = ord (ll [-1])
+	l111l = ll [:-1]
 	
-	l1ll = l11111 % len (l1111)
-	l11l11 = l1111 [:l1ll] + l1111 [l1ll:]
+	l1ll = l111l1 % len (l111l)
+	l11ll1 = l111l [:l1ll] + l111l [l1ll:]
 		
 	if l1:
-		l1l11l = unicode () .join ([unichr (ord (char) - l1l1l1 - (l11l1 + l11111) % l1ll1) for l11l1, char in enumerate (l11l11)])
+		l1l11l = unicode () .join ([unichr (ord (char) - l1l1l1 - (l11ll + l111l1) % l1ll1) for l11ll, char in enumerate (l11ll1)])
 	else:
-		l1l11l = str () .join ([chr (ord (char) - l1l1l1 - (l11l1 + l11111) % l1ll1) for l11l1, char in enumerate (l11l11)])
+		l1l11l = str () .join ([chr (ord (char) - l1l1l1 - (l11ll + l111l1) % l1ll1) for l11ll, char in enumerate (l11ll1)])
 		
 	return eval (l1l11l)
 
@@ -37,16 +37,16 @@ import xbmc
 
 import dixie
 
-l11l1l = l1ll1l (u"ࠫࡵࡲࡵࡨ࡫ࡱ࠲ࡻ࡯ࡤࡦࡱ࠱ࡰ࡮ࡼࡥ࡮࡫ࡻࠫࠀ")
+l11lll = l1ll1l (u"ࠫࡵࡲࡵࡨ࡫ࡱ࠲ࡻ࡯ࡤࡦࡱ࠱ࡰ࡮ࡼࡥ࡮࡫ࡻࠫࠀ")
 
 
 def checkAddons():
-    if l1llll1(l11l1l):
-        l111l1()
+    if l11111(l11lll):
+        l11l11()
 
 
-def l1llll1(l11l1l):
-    if xbmc.getCondVisibility(l1ll1l (u"࡙ࠬࡹࡴࡶࡨࡱ࠳ࡎࡡࡴࡃࡧࡨࡴࡴࠨࠦࡵࠬࠫࠁ") % l11l1l) == 1:
+def l11111(l11lll):
+    if xbmc.getCondVisibility(l1ll1l (u"࡙ࠬࡹࡴࡶࡨࡱ࠳ࡎࡡࡴࡃࡧࡨࡴࡴࠨࠦࡵࠬࠫࠁ") % l11lll) == 1:
         return True
     return False
 
@@ -56,16 +56,16 @@ def getLIVETV(url):
     stream   = url.split(l1ll1l (u"࠭࠺ࠨࠂ"), 1)[-1].lower()
 
     for l1lll1 in l1l1:
-        l11ll = l1lll1[l1ll1l (u"ࠧ࡭ࡣࡥࡩࡱ࠭ࠃ")]
+        l1l11 = l1lll1[l1ll1l (u"ࠧ࡭ࡣࡥࡩࡱ࠭ࠃ")]
         url   = l1lll1[l1ll1l (u"ࠨࡷࡵࡰࠬࠄ")]
 
-        if stream == l11ll.lower():
+        if stream == l1l11.lower():
             return url
 
     return None
 
 
-def l111l1():
+def l11l11():
     l111 = dixie.PROFILE
     PATH = os.path.join(l111, l1ll1l (u"ࠩ࡬ࡲ࡮࠭ࠅ"))
     l11l = l1ll1l (u"ࠪࡰ࡮ࡼࡥࡵࡸ࠱࡭ࡳ࡯ࠧࠆ")
@@ -73,40 +73,40 @@ def l111l1():
 
     l1l1 = getChannels()
 
-    l111ll  = file(l1l, l1ll1l (u"ࠫࡼ࠭ࠇ"))
+    l11l1l  = file(l1l, l1ll1l (u"ࠫࡼ࠭ࠇ"))
 
-    l111ll.write(l1ll1l (u"ࠬࡡࠧࠈ"))
-    l111ll.write(l11l1l)
-    l111ll.write(l1ll1l (u"࠭࡝ࠨࠉ"))
-    l111ll.write(l1ll1l (u"ࠧ࡝ࡰࠪࠊ"))
+    l11l1l.write(l1ll1l (u"ࠬࡡࠧࠈ"))
+    l11l1l.write(l11lll)
+    l11l1l.write(l1ll1l (u"࠭࡝ࠨࠉ"))
+    l11l1l.write(l1ll1l (u"ࠧ࡝ࡰࠪࠊ"))
 
     for l1lll1 in l1l1:
-        l11ll   = l1lll1[l1ll1l (u"ࠨ࡮ࡤࡦࡪࡲࠧࠋ")]
+        l1l11   = l1lll1[l1ll1l (u"ࠨ࡮ࡤࡦࡪࡲࠧࠋ")]
         stream  = l1lll1[l1ll1l (u"ࠩࡸࡶࡱ࠭ࠌ")]
 
-        l111ll.write(l1ll1l (u"ࠪࠩࡸ࠭ࠍ") % l11ll)
-        l111ll.write(l1ll1l (u"ࠫࡂ࠭ࠎ"))
-        l111ll.write(l1ll1l (u"ࠬࡒࡉࡗࡇࡗ࡚࠿࠭ࠏ"))
-        l111ll.write(l1ll1l (u"࠭ࠥࡴࠩࠐ") % l11ll)
-        l111ll.write(l1ll1l (u"ࠧ࡝ࡰࠪࠑ"))
+        l11l1l.write(l1ll1l (u"ࠪࠩࡸ࠭ࠍ") % l1l11)
+        l11l1l.write(l1ll1l (u"ࠫࡂ࠭ࠎ"))
+        l11l1l.write(l1ll1l (u"ࠬࡒࡉࡗࡇࡗ࡚࠿࠭ࠏ"))
+        l11l1l.write(l1ll1l (u"࠭ࠥࡴࠩࠐ") % l1l11)
+        l11l1l.write(l1ll1l (u"ࠧ࡝ࡰࠪࠑ"))
 
-    l111ll.write(l1ll1l (u"ࠨ࡞ࡱࠫࠒ"))
-    l111ll.close()
+    l11l1l.write(l1ll1l (u"ࠨ࡞ࡱࠫࠒ"))
+    l11l1l.close()
 
 
 def getChannels():
-    l1lllll   =  l11lll(l1ll1l (u"ࠩ࡫ࡸࡹࡶࡳ࠻࠱࠲ࡥࡵࡶ࠮ࡶ࡭ࡷࡺࡳࡵࡷ࠯ࡰࡨࡸ࠴ࡼ࠱࠰ࡩࡨࡸࡤࡧ࡬࡭ࡡࡦ࡬ࡦࡴ࡮ࡦ࡮ࡶࠫࠓ"),l1ll1l (u"ࠪ࡫ࡴࡧࡴࠨࠔ"))
+    l1111l   =  l1l111(l1ll1l (u"ࠩ࡫ࡸࡹࡶࡳ࠻࠱࠲ࡥࡵࡶ࠮ࡶ࡭ࡷࡺࡳࡵࡷ࠯ࡰࡨࡸ࠴ࡼ࠱࠰ࡩࡨࡸࡤࡧ࡬࡭ࡡࡦ࡬ࡦࡴ࡮ࡦ࡮ࡶࠫࠓ"),l1ll1l (u"ࠪ࡫ࡴࡧࡴࠨࠔ"))
     headers =  {l1ll1l (u"࡚ࠫࡹࡥࡳ࠯ࡄ࡫ࡪࡴࡴࠨࠕ"):l1ll1l (u"࡛ࠬࡓࡆࡔ࠰ࡅࡌࡋࡎࡕ࠯ࡘࡏ࡙࡜ࡎࡐ࡙࠰ࡅࡕࡖ࠭ࡗ࠳ࠪࠖ"),
                 l1ll1l (u"࠭ࡃࡰࡰࡷࡩࡳࡺ࠭ࡕࡻࡳࡩࠬࠗ"):l1ll1l (u"ࠧࡢࡲࡳࡰ࡮ࡩࡡࡵ࡫ࡲࡲ࠴ࡾ࠭ࡸࡹࡺ࠱࡫ࡵࡲ࡮࠯ࡸࡶࡱ࡫࡮ࡤࡱࡧࡩࡩࡁࠠࡤࡪࡤࡶࡸ࡫ࡴ࠾ࡗࡗࡊ࠲࠾ࠧ࠘"),
                 l1ll1l (u"ࠨࡃࡦࡧࡪࡶࡴ࠮ࡇࡱࡧࡴࡪࡩ࡯ࡩࠪ࠙") : l1ll1l (u"ࠩࡪࡾ࡮ࡶࠧࠚ"),
-                l1ll1l (u"ࠪࡥࡵࡶ࠭ࡵࡱ࡮ࡩࡳ࠭ࠛ"):l1lllll,
+                l1ll1l (u"ࠪࡥࡵࡶ࠭ࡵࡱ࡮ࡩࡳ࠭ࠛ"):l1111l,
                 l1ll1l (u"ࠫࡈࡵ࡮࡯ࡧࡦࡸ࡮ࡵ࡮ࠨࠜ"):l1ll1l (u"ࠬࡑࡥࡦࡲ࠰ࡅࡱ࡯ࡶࡦࠩࠝ"),
                 l1ll1l (u"࠭ࡈࡰࡵࡷࠫࠞ"):l1ll1l (u"ࠧࡢࡲࡳ࠲ࡺࡱࡴࡷࡰࡲࡻ࠳ࡴࡥࡵࠩࠟ")}
     l1lll = {l1ll1l (u"ࠨࡷࡶࡩࡷࡴࡡ࡮ࡧࠪࠠ"):l1ll1l (u"ࠩࡪࡳࡦࡺࠧࠡ")}
 
-    l1111l = requests.post(l1ll1l (u"ࠪ࡬ࡹࡺࡰࡴ࠼࠲࠳ࡦࡶࡰ࠯ࡷ࡮ࡸࡻࡴ࡯ࡸ࠰ࡱࡩࡹ࠵ࡶ࠲࠱ࡪࡩࡹࡥࡡ࡭࡮ࡢࡧ࡭ࡧ࡮࡯ࡧ࡯ࡷࠬࠢ"), data=l1lll, headers=headers)
+    l111ll = requests.post(l1ll1l (u"ࠪ࡬ࡹࡺࡰࡴ࠼࠲࠳ࡦࡶࡰ࠯ࡷ࡮ࡸࡻࡴ࡯ࡸ࠰ࡱࡩࡹ࠵ࡶ࠲࠱ࡪࡩࡹࡥࡡ࡭࡮ࡢࡧ࡭ࡧ࡮࡯ࡧ࡯ࡷࠬࠢ"), data=l1lll, headers=headers)
 
-    l11 =  l1111l.content
+    l11 =  l111ll.content
     l11 =  l11.replace(l1ll1l (u"ࠫࡡ࠵ࠧࠣ"),l1ll1l (u"ࠬ࠵ࠧࠤ"))
     l1l1l = l1ll1l (u"࠭ࠢࡤࡪࡤࡲࡳ࡫࡬ࡠࡰࡤࡱࡪࠨ࠺ࠣࠪ࠱࠯ࡄ࠯ࠢ࠭ࠤ࡬ࡱ࡬ࠨ࠺ࠣࠪ࠱࠯ࡄ࠯ࠢ࠭ࠤ࡫ࡸࡹࡶ࡟ࡴࡶࡵࡩࡦࡳࠢ࠻ࠤࠫ࠲࠰ࡅࠩࠣ࠮ࠥࡶࡹࡳࡰࡠࡵࡷࡶࡪࡧ࡭ࠣ࠼ࠥࠬ࠳࠱࠿ࠪࠤ࠯ࠦࡨࡧࡴࡠ࡫ࡧࠦ࠿ࠨࠨ࠯࠭ࡂ࠭ࠧ࠭ࠥ")
 
@@ -120,19 +120,14 @@ def getChannels():
     return l1l1
 
 
-def l11lll(url, l1ll11):
-    l1l11 = l1ll1l (u"ࠩࡄࡷ࡮ࡧ࠯ࡌࡣࡵࡥࡨ࡮ࡩࠨࠨ")
-    l111l   =  l1llll(l1l11)
-    st   = l1ll1l (u"ࠥࡹࡰࡺࡶ࡯ࡱࡺ࠱ࡹࡵ࡫ࡦࡰ࠰ࠦࠩ")+ l111l + l1ll1l (u"ࠦ࠲ࠨࠪ")+ l1ll1l (u"ࠧࡥࡼࡠ࠯ࠥࠫ") + url + l1ll1l (u"ࠨ࠭ࠣࠬ") + l1ll11 +l1ll1l (u"ࠢ࠮ࠤ࠭") + l1ll1l (u"ࠣࡡࡿࡣࠧ࠮")+ l1ll1l (u"ࠤ࠰ࠦ࠯")+ base64.b64decode(l1ll1l (u"ࠥࡑ࡙ࡏࡺࡏࡆࡘ࠶ࡎ࡛ࡁ࡫ࡌࡆ࡚ࡪࡪࡗࡵ࠲ࡧࡱ࠺ࡼࡤ࠲࠶࡯ࡎࡈࡔࡁࡊࡖ࡜࠵ࡓࡊࡍࡺࡏࡔࡁࡂࠨ࠰"))
-
+def l1l111(url, l1ll11):
+    l11l1   =  l1111()
+    st   = l1ll1l (u"ࠤࡸ࡯ࡹࡼ࡮ࡰࡹ࠰ࡸࡴࡱࡥ࡯࠯ࠥࠨ")+ l11l1 + l1ll1l (u"ࠥ࠱ࠧࠩ")+ l1ll1l (u"ࠦࡤࢂ࡟࠮ࠤࠪ") + url + l1ll1l (u"ࠧ࠳ࠢࠫ") + l1ll11 +l1ll1l (u"ࠨ࠭ࠣࠬ") + l1ll1l (u"ࠢࡠࡾࡢࠦ࠭")+ l1ll1l (u"ࠣ࠯ࠥ࠮")+ base64.b64decode(l1ll1l (u"ࠤࡐࡘࡎࢀࡎࡅࡗ࠵ࡍ࡚ࡇࡪࡋࡅ࡙ࡩࡩ࡝ࡴ࠱ࡦࡰ࠹ࡻࡪ࠱࠵࡮ࡍࡇࡓࡇࡉࡕ࡛࠴ࡒࡉࡓࡹࡎࡓࡀࡁࠧ࠯"))
     return hashlib.md5(st).hexdigest()
 
 
-def l1llll(l1l11):
-    from datetime import datetime
-    from pytz import timezone
+def l1111():
+    from datetime import datetime, timedelta
 
-    l1l111  = timezone(l1l11)
-    l11ll1 = datetime.now(l1l111)
-
-    return l11ll1.strftime(l1ll1l (u"ࠫࠪࡈ࠭ࠦࡦ࠰ࠩ࡞࠭࠱"))
+    l1llll = datetime.now() + timedelta(hours=5)
+    return l1llll.strftime(l1ll1l (u"ࠪࠩࡇ࠳ࠥࡥ࠯ࠨ࡝ࠬ࠰"))
